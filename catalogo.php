3da5e4b6a503310c6e1694895536d451eb07e274
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-04 22:52:42 -0500

    Se creo el catalogo de productos

--- catalogo.php
+++ catalogo.php
@@ -1,3 +1,29 @@
+
+<?php
+    require_once 'conexion.php';
+
+    //consulta de datos
+    $query = "SELECT p.id_producto, p.nombre_producto, p.precio, p.stock, p.foto, c.nombre_categoria FROM producto p INNER JOIN categoria c ON p.id_categoria = c.id_categoria";
+
+    //ejecuto la consulta
+    $result = $conn -> query($query);
+
+    $productos = array();
+    if($result->num_rows > 0){
+      while($row = $result -> fetch_assoc()){
+          array_push($productos, $row['id_producto']);
+          array_push($productos, $row['nombre_producto']);
+          array_push($productos, $row['precio']);
+          array_push($productos, $row['stock']);
+          array_push($productos, $row['foto']);
+          array_push($productos, $row['nombre_categoria']);
+        }
+      
+  }else{
+      echo '<p><em>No existen datos registrados</em></p>';
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -18,71 +44,94 @@
       <div><h1>Encuentra los mejores productos</h1></div>
       <div></div>
       <div class="item">
-        <img src="img/3090GT.png" class="item-img" height="400px" />
+        <?php 
+        echo '<img height="400px" class="item-img" src="data:image/jpeg;base64,'.base64_encode($productos["4"]).'"/>';
+        ?>
+        
         <div class="item-text">
-          <h3>MSI</h3>
+          <?php 
+            echo '<h3>'.$productos[5].'</h3>';
+          ?>
           <p>
-            <a href="facturacion.php" class="enlaces" id="item1">
-              MSI, Nvidia RTX 3090 Gaming Trio USD$1000.99</a
-            >
+            <?php 
+            echo '<a href="facturacion.php?id='.$productos['0'].'" class="enlaces" id="item1">'.$productos[1]. ", USD$ " .$productos[2].'</a>';
+            ?>
+            
           </p>
         </div>
       </div>
       <div class="item">
-        <img src="img/3060EVEA.jpg" class="item-img" height="400px" />
+        <?php 
+        echo '<img height="400px" class="item-img" src="data:image/jpeg;base64,'.base64_encode($productos["10"]).'"/>';
+        ?>
         <div class="item-text">
-          <h3>EVGA</h3>
+          <?php 
+            echo '<h3>'.$productos[11].'</h3>';
+          ?>
           <p>
-            <a href="facturacion.php" class="enlaces" id="item2"
-              >Tarjeta Grefica NVIDIA RTX 3060 TI, USD$679.00</a
-            >
+            <?php 
+            echo '<a href="facturacion.php?id='.$productos['6'].'" class="enlaces" id="item1">'.$productos[7]. ", USD$ " .$productos[8].'</a>';
+            ?>
           </p>
         </div>
       </div>
       <div class="item">
-        <img src="img/logi_G203.png" class="item-img" height="400px" />
+      <?php 
+        echo '<img height="400px" class="item-img" src="data:image/jpeg;base64,'.base64_encode($productos["16"]).'"/>';
+        ?>
         <div class="item-text">
-          <h3>Logitec</h3>
+          <?php 
+            echo '<h3>'.$productos[17].'</h3>';
+          ?>
           <p>
-            <a href="facturacion.php" class="enlaces" id="item3"
-              >Mouse Logitec G203, USD$40.00</a
-            >
+            <?php 
+            echo '<a href="facturacion.php?id='.$productos['12'].'" class="enlaces" id="item1">'.$productos[13]. ", USD$ " .$productos[14].'</a>';
+            ?>
           </p>
         </div>
       </div>
       <div class="item">
-        <img src="img/tecladoLGPro.png" class="item-img" />
+        <?php 
+        echo '<img height="400px" class="item-img" src="data:image/jpeg;base64,'.base64_encode($productos["22"]).'"/>';
+        ?>
         <div class="item-text">
-          <h3>Logitec</h3>
+          <?php 
+            echo '<h3>'.$productos[23].'</h3>';
+          ?>
           <p>
-            <a href="facturacion.php" class="enlaces" id="item4"
-              >Teclado Logitec G PRO, USD$89.99</a
-            >
+            <?php 
+              echo '<a href="facturacion.php?id='.$productos['18'].'" class="enlaces" id="item1">'.$productos[19]. ", USD$ " .$productos[20].'</a>';
+            ?>
           </p>
         </div>
       </div>
       <div class="item">
-        <img src="img/alienware m15R3.jpg" class="item-img" />
+        <?php 
+          echo '<img height="400px" class="item-img" src="data:image/jpeg;base64,'.base64_encode($productos["28"]).'"/>';
+        ?>
         <div class="item-text">
-          <h3>Alienware</h3>
+          <?php 
+            echo '<h3>'.$productos[29].'</h3>';
+          ?>
           <p>
-            <a href="facturacion.php" class="enlaces" id="item5"
-              >Laptop Alienware m15 R3 de 15.6 pulgadas, para juegos FHD (Luna
-              Light) Intel Core i7-10750H, 10ª generación, 16 GB DDR4 RAM, 512
-              GB SSD ,USD$ 1500</a
-            >
+            <?php 
+              echo '<a href="facturacion.php?id='.$productos['24'].'" class="enlaces" id="item1">'.$productos[25]. ", USD$ " .$productos[26].'</a>';
+            ?>
           </p>
         </div>
       </div>
       <div class="item">
-        <img src="img/fp_aw_laptops_hero_1920x1440.webp" class="item-img" />
+        <?php 
+          echo '<img height="400px" class="item-img" src="data:image/jpeg;base64,'.base64_encode($productos["34"]).'"/>';
+        ?>
         <div class="item-text">
-          <h3>Alienware</h3>
+          <?php 
+            echo '<h3>'.$productos[35].'</h3>';
+          ?>
           <p>
-            <a href="facturacion.php" class="enlaces" id="item6"
-              >Laptop Alienware Intel Core i9K, 10ª generación, 16 GB DDR4 RAM,
-              512 GB SSD , USD$1800</a
-            >
+            <?php 
+              echo '<a href="facturacion.php?id='.$productos['30'].'" class="enlaces" id="item1">'.$productos[31]. ", USD$ " .$productos[32].'</a>';
+            ?>
           </p>
         </div>
       </div>
