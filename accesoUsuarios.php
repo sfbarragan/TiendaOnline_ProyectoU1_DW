0dae18546ce5c471a71af87c0d5cc622797193e0
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-07 18:41:56 -0500

    Se agrego el control de acceso para administradores

--- accesoUsuarios.php
+++ accesoUsuarios.php
@@ -19,14 +19,30 @@
         
         //Controlar si llegan los datos
         if($fila['id_cliente'] == null){
-            //Redirigir al mismo index
-            header('location: login.html');
+            //Consulta para verificar si el usuario existe
+            $query2 = "SELECT * FROM admins WHERE email = '$email' AND contra = '$pass'";
+    
+            //Ejecutar consulta
+            $resultado2 = mysqli_query($conn, $query2) or die(mysqli_connect_errno());
+    
+            //Almacenar el resultado de la consulta en un arreglo y tomo el siguiente
+            $fila2 = mysqli_fetch_array($resultado2);
+            
+            //Controlar si llegan los datos
+            if($fila2['id_user'] == null){
+                //Redirigir al mismo login
+                header('location: login.html');
+            }else{
+          
+                header('location: admin.html');
+            }
         }else{
       
             header('location: index.html');
         }
-    }else{
+    }else {
+      
         header('location: login.html');
-    }   
+    }
 
 ?>
