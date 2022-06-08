9758aca6b9ce1d61fb399e0ab23ffe99a6759a8c
sfbarragan <sfbarragan@espe.edu.ec>
2022-06-07 21:01:25 -0500

    Se modifico el acceso usuario

--- accesoUsuarios.php
+++ accesoUsuarios.php
@@ -1,46 +1,47 @@
 <?php
-    //Recibe datos desde el formulario
+    require_once 'conexion.php';
     $email = $_POST['email'];
     $pass = $_POST['contrasenia'];
 
-    if(isset($user)){
-
-
-        session_start();
-        
-        $_SESSION['idusuario'] = $fila['id_ciente'];
-        //Conexion con la base de datos
-        define('SERVERNAME', 'localhost');
-        define('USERNAME', 'root');
-        define('PASSWORD', '');
-        define('DBNAME', 'sistema_ventas');
-
-        //Conexion a la base de datos
-        $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME) or 
-        die('Error al conectar a la base de datos');
-
+    if(isset($email)){
         //Iniciar sesion
         session_start();
-
+        
         //Consulta para verificar si el usuario existe
-        $query = "SELECT * FROM cliente WHERE email = '$email' AND contrasenia = '$pass'";
+        $query1 = "SELECT * FROM cliente WHERE email = '$email' AND contrasenia = '$pass'";
 
         //Ejecutar consulta
-        $resultado = mysqli_query($conn, $query) or die(mysqli_connect_errno());
+        $resultado = mysqli_query($conn, $query1) or die(mysqli_connect_errno());
 
         //Almacenar el resultado de la consulta en un arreglo y tomo el siguiente
         $fila = mysqli_fetch_array($resultado);
         
+
         //Controlar si llegan los datos
         if($fila['id_cliente'] == null){
-            //Redirigir al mismo index
-            header('location: login.html');
+            //Consulta para verificar si el usuario existe
+            $query2 = "SELECT * FROM admins WHERE email = '$email' AND contra = '$pass'";
+
+            //Ejecutar consulta
+            $resultado2 = mysqli_query($conn, $query2) or die(mysqli_connect_errno());
+
+            //Almacenar el resultado de la consulta en un arreglo y tomo el siguiente
+            $fila2 = mysqli_fetch_array($resultado2);
+
+            //Controlar si llegan los datos
+            if($fila2['id_user'] == null){
+                //Redirigir al mismo login
+                header('location: login.html');
+            }else{
+                header('location: admin.php');
+            }
         }else{
-      
+            $_SESSION['idusuario'] = $fila['id_cliente'];
             header('location: index.html');
         }
-    }else{
+    }else {
+
         header('location: login.html');
-    }   
+    }
 
 ?>
